c275728db044885a7bcfb87af9777316d468f157
sakura-rip <root@sakura.rip>
2021-11-06 18:46:30 +0900

    update: uuid format

--- database/weapon.py
+++ database/weapon.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from sqlalchemy import String, Column, Integer
+from sqlalchemy import String, Column, Integer, UniqueConstraint
 from sqlalchemy.orm import Session
 from valorant_api import SyncValorantApi
 
@@ -13,22 +13,20 @@ class Weapon(Base):
     id: str = Column("id", Integer, primary_key=True)
 
     uuid: str = Column("uuid", String, unique=True)
-    lang: str = Column("lang", String, unique=True)
     display_name: str = Column("display_name", String)
     display_icon: str = Column("display_icon", String)
     streamed_video: str = Column("streamed_video", String)
 
     @staticmethod
     def get_promised(session: Session, uuid: str, user: User) -> Weapon:
-        weapon = session.query(Weapon).filter(Weapon.uuid == uuid, Weapon.lang == user.language).first()
+        weapon = session.query(Weapon).filter(Weapon.uuid == (uuid + user.language)).first()
         if weapon is not None:
             return weapon
 
         skin = SyncValorantApi(language=user.language).search_weapon_levels_by_uuid(uuid)
 
         new_weapon = Weapon(
-            uuid=uuid,
-            lang=user.language,
+            uuid=uuid+user.language,
             display_name=skin.display_name,
             display_icon=skin.display_icon,
             streamed_video=skin.streamed_video
