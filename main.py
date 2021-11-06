9542f307c7686a183303601805da9b764439bbd3
sakura-rip <root@sakura.rip>
2021-11-06 13:28:10 +0900

    delete: intent

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import discord
 from client import ValorantStoreBot
 
 if __name__ == "__main__":
-    bot = ValorantStoreBot("", discord.Intents.all())
+    bot = ValorantStoreBot("" )
     bot.run(os.getenv("DISCORD_TOKEN"))
