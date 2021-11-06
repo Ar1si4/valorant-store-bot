c505ebf7a9bdf9b7c8ad39c7f2742be5412fcda0
sakura-rip <root@sakura.rip>
2021-11-06 14:31:53 +0900

    add: language function

--- database/user.py
+++ database/user.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Integer
+from __future__ import annotations
+
+from sqlalchemy import Column, Integer, String
 
 
 class User:
     __tablename__ = "users"
 
     id: int = Column("id", Integer, primary_key=True)
+    language: str = Column("language", String)
+
     @staticmethod
     def get_promised(session, id: int) -> User:
         user = session.query(User).filter(User.id == id).first()
@@ -14,3 +18,8 @@ class User:
         session.add(new_user)
         session.commit()
         return new_user
+
+    def get_text(self, ja: str, en: str):
+        if self.language == "ja-JP":
+            return ja
+        return en
