e2c82227d88968575c2f82890106fba7fa2dbedb
sakura-rip <root@sakura.rip>
2021-11-10 21:41:27 +0900

    change: move directory

--- cogs/proxies.py
+++ cogs/proxies.py
@@ -3,19 +3,18 @@ import random
 
 import sqlalchemy.orm.scoping
 
-from database import User
 from database.user import RiotAccount
 
 with open("proxies.json", "r", encoding="utf-8") as f:
     ips = json.load(f)
 
 
-def get_ip(session: sqlalchemy.orm.Session, user: User, account: RiotAccount):
-    if user.is_premium:
+def get_ip(session: sqlalchemy.orm.Session, is_premium: bool, account: RiotAccount):
+    if is_premium:
         if not account.proxy_ip:
             new_ip = random.choice(ips[6000:])
             if session.query(RiotAccount).filter(RiotAccount.proxy_ip == new_ip).count() != 0:
-                return get_ip(session, user, account)
+                return get_ip(session, is_premium, account)
             account.proxy_ip = new_ip
             session.commit()
             return new_ip
@@ -24,12 +23,12 @@ def get_ip(session: sqlalchemy.orm.Session, user: User, account: RiotAccount):
                 return account.proxy_ip
             account.proxy_ip = ""
             session.commit()
-            return get_ip(session, user, account)
+            return get_ip(session, is_premium, account)
     return random.choice(ips[:14000])
 
 
-def get_proxy_url(session: sqlalchemy.orm.Session, user: User, account: RiotAccount):
-    ip = get_ip(session, user, account)
+def get_proxy_url(session: sqlalchemy.orm.Session, is_premium: bool, account: RiotAccount):
+    ip = get_ip(session, is_premium, account)
     return {
         "http": f"http://lum-customer-hl_e6fa5f6e-zone-valorant_store_bot-ip-{ip}:i8j8xfmwebi0@zproxy.lum-superproxy.io:22225",
         "https": f"https://lum-customer-hl_e6fa5f6e-zone-valorant_store_bot-ip-{ip}:i8j8xfmwebi0@zproxy.lum-superproxy.io:22225"
