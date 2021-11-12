7ca4f3d5b27272adba8625a4f1c15958616c15bc
sakura-rip <root@sakura.rip>
2021-11-12 22:55:12 +0900

    update: package

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,5 @@
 from .guild import Guild
-from .user import User
+from .user import User, RiotAccount
 from .weapon import Weapon
 from .skin_log import SkinLog
 from .setting import Base, ENGINE, session
