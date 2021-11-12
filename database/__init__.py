0d349f2e9eba3532f2b4ca131892fcf347139598
sakura-rip <root@sakura.rip>
2021-11-12 16:36:41 +0900

    change: to one field

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,7 @@
 from .guild import Guild
 from .user import User
 from .weapon import Weapon
+from .skin_log import SkinLog
 from .setting import Base, ENGINE, session
 
 Base.metadata.create_all(bind=ENGINE)
--- database/skin_log.py
+++ database/skin_log.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, DATETIME
+from sqlalchemy import Column, Integer, String, DATE
 
 from .setting import Base
 
@@ -11,8 +11,5 @@ class SkinLog(Base):
     id: int = Column("id", Integer, autoincrement=True, primary_key=True)
 
     account_puuid: str = Column("account_puuid", String)
-    date: datetime.datetime = Column("date", DATETIME)
-    skin_1: str = Column("skin_1", String)
-    skin_2: str = Column("skin_2", String)
-    skin_3: str = Column("skin_3", String)
-    skin_4: str = Column("skin_4", String)
+    date: datetime.date = Column("date", DATE)
+    skin_uuid: str = Column("skin_uuid", String)
